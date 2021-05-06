12889f0d78558ccbf3674e1d7ca1a4188ec6703a
Alexandros Raikos <alexandros@araikos.gr>
2021-05-06 14:07:47 +0300

    added package info

---
+++ woobiz.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name:       WooBiz
+ * Plugin URI:        https://github.com/alexandrosraikos/woobiz
+ * Description:       A plugin designed to integrate WooCommerce with the BizCourier logistics platform.
+ * Version:           0.0.1
+ * Requires at least: 5.7
+ * Requires PHP:      7.4
+ * Author:            Alexandros Raikos
+ * Author URI:        https://www.araikos.gr/
+ * License:           GNU General Public License v3.0
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       woobiz
+ * Domain Path:       /languages
+ */
