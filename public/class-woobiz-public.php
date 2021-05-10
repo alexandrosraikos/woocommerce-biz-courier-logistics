7f796c78993d29682b4b507524f2f5e096ccf128
Alexandros Raikos <alexandros@araikos.gr>
2021-05-10 15:05:25 +0300

    init boilerplate

---
+++ public/class-woobiz-public.php
@@ -0,0 +1,103 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    WooBiz
+ * @subpackage WooBiz/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @package    WooBiz
+ * @subpackage WooBiz/public
+ * @author     Your Name <email@example.com>
+ */
+class WooBiz_Public {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $WooBiz    The ID of this plugin.
+	 */
+	private $WooBiz;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $WooBiz       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $WooBiz, $version ) {
+
+		$this->WooBiz = $WooBiz;
+		$this->version = $version;
+
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in WooBiz_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The WooBiz_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->WooBiz, plugin_dir_url( __FILE__ ) . 'css/woobiz-public.css', array(), $this->version, 'all' );
+
+	}
+
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in WooBiz_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The WooBiz_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( $this->WooBiz, plugin_dir_url( __FILE__ ) . 'js/woobiz-public.js', array( 'jquery' ), $this->version, false );
+
+	}
+
+}
--- woobiz.php
+++ woobiz.php
@@ -1,14 +1,71 @@
+<?php 
 /**
  * Plugin Name:       WooBiz
  * Plugin URI:        https://github.com/alexandrosraikos/woobiz
- * Description:       A plugin designed to integrate WooCommerce with the BizCourier logistics platform.
- * Version:           0.0.1
+ * Description:       Ενσωματώστε το WooCommerce με την πλατφόρμα BizCourier.
+ * Version:           1.0.0
  * Requires at least: 5.7
  * Requires PHP:      7.4
- * Author:            Alexandros Raikos
+ * Author:            Αλέξανδρος Ράικος
  * Author URI:        https://www.araikos.gr/
  * License:           GNU General Public License v3.0
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       woobiz
  * Domain Path:       /languages
  */
+
+ // If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'WOOBIZ_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-woobiz-activator.php
+ */
+function activate_woobiz() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-woobiz-activator.php';
+	WooBiz_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-woobiz-deactivator.php
+ */
+function deactivate_woobiz() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-woobiz-deactivator.php';
+	WooBiz_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_woobiz' );
+register_deactivation_hook( __FILE__, 'deactivate_woobiz' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-woobiz.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_woobiz() {
+
+	$plugin = new WooBiz();
+	$plugin->run();
+
+}
+run_woobiz();
